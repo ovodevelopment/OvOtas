d9c6d628053126b04aeff80d0245140224002eb2
packjack <83289186+Packjackisback@users.noreply.github.com>
2024-01-15 14:49:17 -0600

    Update savestate.js

--- 1.4.4/savestate.js
+++ 1.4.4/savestate.js
@@ -1,11 +1,11 @@
-var temp;
-var my_lzma = new LZMA("lzma.js");
+let temp;
+
 // Add an event listener to the document for a keydown event
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', (event) => {
   // Check if the pressed key is the '<' key (key code 188) or use event.key === '<' for modern browsers
   if (event.keyCode === 188 || event.key === '<') {
     // Call the c2_callFunction
-    c2_callFunction("Menu > DownloadReplay", []);
+    c2_callFunction('Menu > DownloadReplay', []);
 
     // Save 'url_' to 'temp'
     temp = url_;
@@ -13,8 +13,9 @@ document.addEventListener('keydown', function(event) {
     // Log the value of 'temp'
     console.log('Variable temp:', temp);
 
-    //decompress with lzma
+    // Decompress with lzma
     const compressedArray = new Uint8Array(temp);
+
     // Decompress the data using lzma
     lzma.decompress(compressedArray, (result, error) => {
       if (error) {
@@ -24,7 +25,6 @@ document.addEventListener('keydown', function(event) {
         const decryptedData = result.toString('utf-8');
         console.log('Decrypted Data:', decryptedData);
       }
-    
-
+    });
   }
 });
